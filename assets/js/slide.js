8bc11825ca25f9f3618e54451f732b8da2bbf562
yiinuma <y.iinuma211@gmail.com>
2020-12-06 17:21:57 +0900

    slide　内包化

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,29 +1,31 @@
-$(function () {
-  // 「#sidebar-button--open」のclickイベント→「#sidebar」を開く
-  $("#sidebar-button--open").on("click", function () {
-    $("#sidebar").addClass("is-open");
-    $(".p-sidebar__back").addClass("is-open");
-    $("body").addClass("is-open");
-  });
+jQuery(function ($) {
+  $(function () {
+    // 「#sidebar-button--open」のclickイベント→「#sidebar」を開く
+    $("#sidebar-button--open").on("click", function () {
+      $("#sidebar").addClass("is-open");
+      $(".p-sidebar__back").addClass("is-open");
+      $("body").addClass("is-open");
+    });
 
-  // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
-  $("#sidebar-button--close").on("click", function () {
-    $("#sidebar").removeClass("is-open");
-    $(".p-sidebar__back").removeClass("is-open");
-    $("body").removeClass("is-open");
-  });
-
-  // 画面サイズがPC以上になったらサイドバーを開いた状態でもPC用のデフォルト画面に戻す
-  var $win = $(window);
-
-  $win.on("load resize", function () {
-    var windowWidth = window.innerWidth;
-
-    if (windowWidth >= 961) {
-      // PCの処理
+    // 「#js_close_button」のclickイベント→「#sidebar」を閉じる
+    $("#sidebar-button--close").on("click", function () {
       $("#sidebar").removeClass("is-open");
       $(".p-sidebar__back").removeClass("is-open");
       $("body").removeClass("is-open");
-    }
+    });
+
+    // 画面サイズがPC以上になったらサイドバーを開いた状態でもPC用のデフォルト画面に戻す
+    var $win = $(window);
+
+    $win.on("load resize", function () {
+      var windowWidth = window.innerWidth;
+
+      if (windowWidth >= 961) {
+        // PCの処理
+        $("#sidebar").removeClass("is-open");
+        $(".p-sidebar__back").removeClass("is-open");
+        $("body").removeClass("is-open");
+      }
+    });
   });
 });
