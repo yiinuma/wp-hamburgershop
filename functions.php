609c6613c811503b043dbb68329509d65e583d68
yiinuma <y.iinuma211@gmail.com>
2020-12-06 16:44:56 +0900

    functions修正

--- functions.php
+++ functions.php
@@ -5,15 +5,15 @@
     add_theme_support( 'post-thumbnails' );
 
     //タイトル出力
-    function hamburger_title( $title ) {
-        if ( is_front_page() && is_home() ) { //トップページなら
-            $title = get_bloginfo( 'name','display' );
-        } elseif ( is_singular() ) { //シングルページなら
-            $title = single_post_title( '', false );
-        }
-        return $title;
-    }
-    add_filter( 'pre_get_document_title', 'hamburger_title' );
+    // function hamburger_title( $title ) {
+    //     if ( is_front_page() && is_home() ) { //トップページなら
+    //         $title = get_bloginfo( 'name','display' );
+    //     } elseif ( is_singular() ) { //シングルページなら
+    //         $title = single_post_title( '', false );
+    //     }
+    //     return $title;
+    // }
+    // add_filter( 'pre_get_document_title', 'hamburger_title' );
 
     function hamburger_script() {
         wp_enqueue_style('font-awesome','//use.fontawesome.com/releases/v5.8.2/css/all.css',array());
